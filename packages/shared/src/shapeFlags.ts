6d6fb2d5de8a5a62983b9ed8b2077cd5dd4c3134
清咔 <874518796@qq.com>
2024-05-20 23:50:28 +0800

    1

--- packages/shared/src/shapeFlags.ts
+++ packages/shared/src/shapeFlags.ts
@@ -1,13 +1,13 @@
 export enum ShapeFlags {
-  ELEMENT = 1,
-  FUNCTIONAL_COMPONENT = 1 << 1,
-  STATEFUL_COMPONENT = 1 << 2,
-  TEXT_CHILDREN = 1 << 3,
-  ARRAY_CHILDREN = 1 << 4,
-  SLOTS_CHILDREN = 1 << 5,
-  TELEPORT = 1 << 6,
-  SUSPENSE = 1 << 7,
-  COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,
-  COMPONENT_KEPT_ALIVE = 1 << 9,
+  ELEMENT = 1,      // 元素
+  FUNCTIONAL_COMPONENT = 1 << 1,// 10 => 2            功能性组件
+  STATEFUL_COMPONENT = 1 << 2,  // 100 => 4           状态组件
+  TEXT_CHILDREN = 1 << 3,       //  1000 => 8         文本子节点
+  ARRAY_CHILDREN = 1 << 4,      //  10000 => 16       遍历的组件
+  SLOTS_CHILDREN = 1 << 5,      //  100000 => 32      槽内的组件
+  TELEPORT = 1 << 6,            //  1000000 => 64     Teleport 组件
+  SUSPENSE = 1 << 7,            //  10000000 => 128   Suspense 组件
+  COMPONENT_SHOULD_KEEP_ALIVE = 1 << 8,  //   100000000 => 256   需要缓存的组件
+  COMPONENT_KEPT_ALIVE = 1 << 9,          //  1000000000 => 512  已经缓存的组件
   COMPONENT = ShapeFlags.STATEFUL_COMPONENT | ShapeFlags.FUNCTIONAL_COMPONENT,
 }
